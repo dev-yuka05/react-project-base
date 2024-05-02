cf4fd740cc854fdedc7f09a58ebf1deb798c2d42
Minjun <84955607+dev-yuka05@users.noreply.github.com>
2024-05-02 13:24:24 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { PrimeReactProvider } from 'primereact/api';
 
 // GlobalStyles
 import GlobalStyles from './GlobalStyles';
 
 // pages
-import Main from './pages/Main';
 
 const App = () => {
   return (
-    <PrimeReactProvider>
-      <BrowserRouter>
-        <GlobalStyles />
-				<Routes>
-					<Route path="/" element={<Main />}></Route>
-				</Routes>
-			</BrowserRouter>
-    </PrimeReactProvider>
+    <BrowserRouter>
+      <GlobalStyles />
+      <Routes>
+	
+        <Route path="/" element={<Main />}></Route>
+	
+      </Routes>
+    </BrowserRouter>
   );
 }
 
