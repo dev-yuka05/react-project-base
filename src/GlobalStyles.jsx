1294a250b3a26b2412c7b36e4dafb95159324f75
Minjun <84955607+dev-yuka05@users.noreply.github.com>
2024-05-02 13:24:59 +0900

    Update GlobalStyles.jsx

--- src/GlobalStyles.jsx
+++ src/GlobalStyles.jsx
@@ -1,16 +1,17 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
-	*{
-    margin: 0;
-    padding: 0;
+ *{
     font-family: 'Noto Sans KR', sans-serif;
     box-sizing: border-box;
   }
 
   html, body {
     font-size: 14px;
+    margin: 0;
+    padding: 0;
   }
+
 `
 
 export default GlobalStyles;
