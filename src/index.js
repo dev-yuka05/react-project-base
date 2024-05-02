af0ef8ca805a26db992cc671b9494159051f6695
Minjun <84955607+dev-yuka05@users.noreply.github.com>
2024-05-02 13:25:22 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'primereact/resources/themes/lara-light-indigo/theme.css';   // theme
-import 'primereact/resources/primereact.css';                       // core css
-import 'primeicons/primeicons.css';                                 // icons
-import 'primeflex/primeflex.css';                                   // css utility
-import App from './App';
+import App from "./App";
+import { PrimeReactProvider } from 'primereact/api';
+import 'primereact/resources/primereact.css';
+import "primereact/resources/themes/md-light-indigo/theme.css";
+import 'primeflex/primeflex.css';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fas);
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
   </React.StrictMode>
 );
